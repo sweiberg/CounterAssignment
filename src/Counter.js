621000447f86a32917224fc1406e81c58cb375d0
sweiberg <101523888+sweiberg@users.noreply.github.com>
2023-03-10 17:46:58 -0500

    Initial commit

---
+++ src/Counter.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  function increment() {
+    // Increment the counter
+    setCount(count + 1);
+  }
+  function decrement() {
+    // Decrement the counter
+    setCount(count - 1);
+  }
+  return (
+    <div>
+      <h2>Counter Assignment</h2>
+      <p>{count}</p>
+      <button onClick={increment}> + </button>
+      <button onClick={decrement}> - </button>
+    </div>
+  );
+}
